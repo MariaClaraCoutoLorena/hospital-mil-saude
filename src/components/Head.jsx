858baf7101316cff8442efd6b8aadd5973cd6333
Maria Clara Couto Lorena <mariaclaracoutolorena@gmail.com>
2025-07-12 18:29:17 -0300

    ajuste de chamada

--- src/components/Head.jsx
+++ src/components/Head.jsx
@@ -1,32 +1,7 @@
 import avatar from '../assets/avatar.png'
-import { useEffect, useState } from 'react';
-import { database } from '../config/firebase-config';
-import { getDocs, getDoc, collection, doc } from 'firebase/firestore';
 
-
-function Head() {
-
-    const [medico, setMedico] = useState({});
-
-    const id = 'cXrZqbso4inFPbFHbD8f';
-    const dbRef = doc(database, 'medicos', id);
-  
-    useEffect(() => {
-      const getDoctorById = async () => {
-          const docMedico = await getDoc(dbRef);
-      
-          if (docMedico.exists()) {
-            setMedico({ ...docMedico.data(), id: docMedico.id });
-          } else {
-            console.log("Médico não encontrado");
-          }
-      };
-
-      getDoctorById();
-
-    }, []);
-      
-
+function Head(props) {
+    const medico = props.medico
     return (
         <div className="head">
 
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,30 +1,36 @@
 import { useEffect, useState } from 'react';
 import { database } from '../config/firebase-config';
-import { getDocs, collection } from 'firebase/firestore';
+import { getDoc, collection, doc } from 'firebase/firestore';
 import Footer from '../components/Footer';
 import Head from '../components/Head';
 import ButtonCard from '../components/ButtonCard';
 
 function Home() {
-  const [medicos, setMedicos] = useState([]);
-
-  const dbRef = collection(database, 'medicos');
-
+  const [medico, setMedico] = useState({});
+  
+  const id = 'cXrZqbso4inFPbFHbD8f';
+  
+  const dbRef = doc(database, 'medicos', id);
+    
   useEffect(() => {
-    const getDoctors = async () => {
-      const data = await getDocs(dbRef);
-      const filter = data.docs.map((doc) => ({
-        ...doc.data(),
-        id: doc.id,
-      }));
-      setMedicos(filter); 
+    const getDoctorById = async () => {
+      const docMedico = await getDoc(dbRef);
+  
+      if (docMedico.exists()) {
+        setMedico({ ...docMedico.data(), id: docMedico.id });
+      } else {
+        console.log("Médico não encontrado");
+      }
     };
-    getDoctors();
+  
+    getDoctorById();
+  
   }, []);
+        
 
   return (
     <>
-      <Head/>
+      <Head medico_nome={medico.nome} medico_crm={medico.crm}> </Head>
       <ButtonCard route_direct="/consultas">Consultar Histórico de Consultas</ButtonCard>
       <ButtonCard route_direct="/doencas">Consultar CID de Doenças</ButtonCard>
       <Footer/>
