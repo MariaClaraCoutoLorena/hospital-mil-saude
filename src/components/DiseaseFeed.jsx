af934c08505bdb3dfe5d26bd91dce18fc73ed626
Maria Clara Couto Lorena <mariaclaracoutolorena@gmail.com>
2025-07-12 23:00:16 -0300

    filtro de pesquisa funcionando

--- src/components/DiseaseFeed.jsx
+++ src/components/DiseaseFeed.jsx
@@ -12,12 +12,26 @@ function DiseaseFeed(props) {
     () => {
       const getDoencas = async () =>{
         if(props.symptom.includes('*') || props.symptom.length == 0) {
+          if(props.nameSearch==""){
+            var returnedDocs = await getDocs(dbRef);
+          } else{
+            query_result = query(dbRef, where("nome", "==", props.nameSearch));
+            var returnedDocs = await getDocs(query_result);
+          }
           
-          var returnedDocs = await getDocs(dbRef);
-          
-        }else{
-
-          const query_result = query(dbRef, where("sintomas", "array-contains-any", props.symptom));
+        } else {
+          console.log(props)
+          var query_result = ""
+          if(props.nameSearch==""){
+            query_result = query(dbRef, where("sintomas", "array-contains-any", props.symptom));
+            console.log("Sintomas filtrados")
+          } else{
+            query_result = query(dbRef, 
+                                      where("sintomas", "array-contains-any", props.symptom),
+                                      where("nome", "==", props.nameSearch)
+                                    );
+            console.log("Nome pesquisado")
+          }
           var returnedDocs = await getDocs(query_result);
         }
 
@@ -38,7 +52,7 @@ function DiseaseFeed(props) {
 
 
   return (
-    <div class='diseasefeed'>
+    <div className='diseasefeed'>
         {doencas.map(
           (d, index) => (<DiseaseCard key={index}name={d.nome} cid={d.cid} symptoms={d.sintomas.map((sintoma,i)=>i!=d.sintomas.length-1 ? sintoma + ', ' : sintoma)}></DiseaseCard>)
         )}
